1e40d329582766d472e3490063b52117bfa5a071
Daniil Litvinenko <danil.fire@bk.ru>
2023-04-05 01:18:17 +0700

    Create InputSystem

---
+++ Assets/Core/Input/CharacterInput.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class CharacterInput : MonoBehaviour
+{
+    private GameInput _gameInput;
+
+    private void Awake()
+    {
+        _gameInput = new GameInput();
+        _gameInput.Enable();
+    }
+
+    private void Update()
+    {
+        ReadMovement();
+    }
+
+    private void ReadMovement()
+    {
+        var inputDirection = _gameInput.Gameplay.Movement.ReadValue<Vector2>();
+
+        // Передаём движение
+    }
+}
