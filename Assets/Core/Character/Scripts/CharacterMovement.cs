1b10fc5cea8f42fff43fc8cff01d44f239cdd6a0
Ilyas Atabaev <ilyasatabaev731@gmail.com>
2023-04-05 18:56:36 +0500

    add character movement and 3d model with animations

---
+++ Assets/Core/Character/Scripts/CharacterMovement.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    [SerializeField] private CharacterInput _input;
+    [SerializeField] private float _speed = 5;
+    [SerializeField] private float _rotationSpeed = 5;
+
+    private Vector3 _lastDeltaPos; 
+    
+    private CharacterStates State
+    {
+        get => (CharacterStates)_animator.GetInteger("state");
+        set => _animator.SetInteger("state", (int)value);
+    }
+
+    private void Run(Vector3 direction)
+    {
+        State = CharacterStates.Run;
+        
+        Vector3 deltaPos = direction * _speed * Time.deltaTime;
+
+        transform.position += deltaPos;
+        
+        if (deltaPos.magnitude > 0)
+        {
+            _lastDeltaPos = deltaPos;
+        }
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(_lastDeltaPos), Time.deltaTime * _rotationSpeed);
+    }
+
+    private void Idle()
+    {
+        State = CharacterStates.Idle;
+    }
+
+    private void Update()
+    {
+        Vector3 direction = _input.GetDirection();
+        direction.z = direction.y;
+        direction.y = 0;
+
+        if (direction.magnitude > 0)
+        {
+            Run(direction);
+        }
+        else
+        {
+            Idle();
+        }
+    }
+}
+
+enum CharacterStates
+{
+    Idle,
+    Run
+}
