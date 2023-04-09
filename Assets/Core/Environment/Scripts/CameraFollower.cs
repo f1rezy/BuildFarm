69955d99d78ab318fc0a32a3698a4bbef9bed7fc
Daniil Litvinenko <danil.fire@bk.ru>
2023-04-10 01:12:58 +0700

    Create camera follower

---
+++ Assets/Core/Environment/Scripts/CameraFollower.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    [SerializeField] private Vector3 _offcet;
+    [SerializeField] private float _smoothing = 3f;
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var nextPosition = Vector3.Lerp(transform.position, _target.position + _offcet, Time.fixedDeltaTime * _smoothing);
+
+        transform.position = nextPosition;
+    }
+}
