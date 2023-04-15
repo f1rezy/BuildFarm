788108de6473a9ff702437dcc664cb793a0e7906
Anton Knapp <antonk3344@gmail.com>
2023-04-15 17:59:46 +0300

    Add flowers as enviromental objects

--- Assets/Core/Environment/On Grid Objects/Scripts/GridEnviromentalObject.cs
+++ Assets/Core/Environment/On Grid Objects/Scripts/GridEnviromentalObject.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TreeOnGrid : MonoBehaviour
+public class GridEnviromentalObject : MonoBehaviour
 {
     [SerializeField] private BuildableObjectGrid _grid;
 
